fd2f9474be182ac120b6ef865ccab87284db74dd
Arno Huet <ah2276@cornell.edu>
2026-05-04 00:24:52 -0400

    actual fix tolevel progress; i swear this one will work

--- core/src/main/java/edu/cornell/cis3152/physics/screen/levels/LevelProgress.java
+++ core/src/main/java/edu/cornell/cis3152/physics/screen/levels/LevelProgress.java
@@ -1,14 +1,10 @@
 package edu.cornell.cis3152.physics.screen.levels;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.utils.JsonValue;
 import com.badlogic.gdx.utils.JsonWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Reader;
-import java.io.Writer;
 
 public class LevelProgress {
 
@@ -86,20 +82,20 @@ public class LevelProgress {
     public void saveGame() {
         Json js = new Json();
         js.setOutputType(JsonWriter.OutputType.json);
-        try (Writer writer = new FileWriter(saveGameFile)) {
+        try {
             String pretty = js.prettyPrint(js.toJson(levels, Array.class, LevelData.class));
-            writer.write(pretty);
+            Gdx.files.local(saveGameFile).writeString(pretty, false);
         } catch (Exception e) {
             System.out.println("Error saving game");
-            resetSaveGame();
         }
     }
 
     public void loadGame(){
         Json js = new Json();
         js.setOutputType(JsonWriter.OutputType.json);
-        try (Reader reader = new FileReader(saveGameFile)) {
-            levels = js.fromJson(Array.class, LevelData.class, reader);
+        try {
+            String content = Gdx.files.local(saveGameFile).readString();
+            levels = js.fromJson(Array.class, LevelData.class, content);
         } catch (Exception e) {
             System.out.println("Error loading game");
             resetSaveGame();
